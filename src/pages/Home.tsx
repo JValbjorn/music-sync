050e610b7f12858595eec5897d56f7bf915a5cd1
JValbjorn <julievalbjorn@live.dk>
2025-09-09 21:39:20 +0200

    Add react router and corrected file type

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
-export default function Home() {
+import type { JSX } from "react";
+
+
+export default function Home() : JSX.Element {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
